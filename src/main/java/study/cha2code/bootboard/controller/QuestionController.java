ff775317a3bfa9d258e94cb07a417d2059197911
cha2jini <cha2jini@naver.com>
2024-01-26 23:57:53 +0900

    🚧 Question 목록 출력
    - DB에 저장한 질문 목록 단순 출력 테스트

--- src/main/java/study/cha2code/bootboard/controller/QuestionController.java
+++ src/main/java/study/cha2code/bootboard/controller/QuestionController.java
@@ -1,21 +1,34 @@
 package study.cha2code.bootboard.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import study.cha2code.bootboard.entity.Question;
+import study.cha2code.bootboard.repository.QuestionRepository;
+
+import java.util.List;
 
 /**
  * Question list를 매핑하기 위한 controller
  */
 @Controller
+// final이 붙은 속성을 생성자로 객체 주입 (= QuestionRepository)
+@RequiredArgsConstructor
 @RequestMapping("/question")
 public class QuestionController {
 
+	private final QuestionRepository repository;
+
 	@GetMapping("/list")
-	// JSON 형식으로 응답해주는 어노테이션
-	@ResponseBody
-	public String list() {
-		return "question list";
+	public String list(Model model) {
+
+		// Question list 생성 및 검색
+		List<Question> questionList = this.repository.findAll();
+		// 생성 된 questionList를 model에 저장 (template에서 값을 사용하기 위함)
+		model.addAttribute("questionList", questionList);
+
+		return "questionList";
 	}
 }
